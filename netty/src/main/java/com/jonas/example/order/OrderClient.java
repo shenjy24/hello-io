23b3af0fd96634b35a7a0f77cac39919addcaa6d
shenjy <shenjy24@gmail.com>
2023-03-21 17:55:35 +0800

    handler的执行顺序

---
+++ netty/src/main/java/com/jonas/example/order/OrderClient.java
@@ -0,0 +1,35 @@
+package com.jonas.example.order;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class OrderClient {
+    public void connect(String host, int port) {
+        //配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new OrderClientChannelInitializer());
+            //发起异步连接操作
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            //等待客户端链路关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new OrderClient().connect("127.0.0.1", 8080);
+    }
+}
